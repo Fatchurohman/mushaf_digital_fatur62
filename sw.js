7fdd3af33ae2ac145b904a4646b603a40a783322
Fatchurohman <fatur62@gmail.com>
2026-09-21 06:30:19 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,59 @@
-kosong
+const CACHE_NAME = 'mushaf-fatur62-v1';
+
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './manifest.json',
+    './data/surah-mapping.json',
+    './data/surah/36.json',
+    './data/surah/1.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('[Service Worker] Caching static assets...');
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+            .catch((err) => console.error('[Service Worker] Cache fail:', err))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((cachedResponse) => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+                return fetch(event.request).then((networkResponse) => {
+                    if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+                        return networkResponse;
+                    }
+                    const responseToCache = networkResponse.clone();
+                    caches.open(CACHE_NAME).then((cache) => {
+                        cache.put(event.request, responseToCache);
+                    });
+                    return networkResponse;
+                });
+            })
+    );
+});
+
